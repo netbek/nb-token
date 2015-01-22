341463302befe8de0268e242bafa7c6fd23dc5d2
Hein Bekker <hein@netbek.co.za>
2015-01-22 17:17:18 +0200

    Replace extend with merge

--- dist/js/nb-token.js
+++ dist/js/nb-token.js
@@ -29,7 +29,7 @@
 		};
 		return {
 			set: function (values) {
-				config = extend(true, {}, config, values);
+				config = window.merge(true, config, values);
 			},
 			$get: function () {
 				return config;
@@ -175,91 +175,5 @@
 				}
 			]
 		};
-	}
-
-	/**
-	 * Checks if value is an object created by the Object constructor.
-	 *
-	 * @param {mixed} value
-	 * @returns {Boolean}
-	 */
-	function isPlainObject (value) {
-		return (!!value && typeof value === 'object' && value.constructor === Object
-			// Not DOM node
-			&& !value.nodeType
-			// Not window
-			&& value !== value.window);
-	}
-
-	/**
-	 * Merge the contents of two or more objects together into the first object.
-	 *
-	 * Shallow copy: extend({}, old)
-	 * Deep copy: extend(true, {}, old)
-	 *
-	 * Based on jQuery (MIT License, (c) 2014 jQuery Foundation, Inc. and other contributors)
-	 */
-	function extend () {
-		var options, key, src, copy, copyIsArray, clone,
-			target = arguments[0] || {},
-			i = 1,
-			length = arguments.length,
-			deep = false;
-
-		// Handle a deep copy situation
-		if (typeof target === 'boolean') {
-			deep = target;
-
-			// Skip the boolean and the target
-			target = arguments[i] || {};
-			i++;
-		}
-
-		// Handle case when target is a string or something (possible in deep copy)
-		if (!isPlainObject(target) && !angular.isFunction(target)) {
-			target = {};
-		}
-
-		// If only one argument is passed
-		if (i === length) {
-			i--;
-		}
-
-		for (; i < length; i++) {
-			// Only deal with non-null/undefined values
-			if ((options = arguments[i]) != null) {
-				// Extend the base object
-				for (key in options) {
-					src = target[key];
-					copy = options[key];
-
-					// Prevent never-ending loop
-					if (target === copy) {
-						continue;
-					}
-
-					// Recurse if we're merging plain objects or arrays
-					if (deep && copy && (isPlainObject(copy) || (copyIsArray = angular.isArray(copy)))) {
-						if (copyIsArray) {
-							copyIsArray = false;
-							clone = src && angular.isArray(src) ? src : [];
-						}
-						else {
-							clone = src && isPlainObject(src) ? src : {};
-						}
-
-						// Never move original objects, clone them
-						target[key] = extend(deep, clone, copy);
-					}
-					// Don't bring in undefined values
-					else if (copy !== undefined) {
-						target[key] = copy;
-					}
-				}
-			}
-		}
-
-		// Return the modified object
-		return target;
 	}
 })(window, window.angular);
